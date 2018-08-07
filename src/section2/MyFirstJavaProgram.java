1208363c10416582476e2ce2dbee82fb76548b5c
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-07 11:55:34 -0700

    Day2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,18 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot   BB8=new  Robot();
+	BB8.changeRobot("https://i.ytimg.com/vi/CyH09RLM9Dk/maxresdefault.jpg");
+		BB8.penDown();
+		BB8.setRandomPenColor();
+		int sides=17;
+		int angle=360/sides;	
+	for(int i=0;i<sides;i++) {
+		BB8.move(100);
+		BB8.turn(angle);
+	}
 	
-		
-		
+	
+	
 	}
 }
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,23 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+	Robot bb8=new Robot();
 		// 5. Set your robot's pen down 
-		
+		bb8.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+		bb8.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
+		for (int i = 5; i < 75; i++) {
+			
 		
 			// 7. Change the pen color to random
-		
+		bb8.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			
+			bb8.move(5*i);
 			// 2. Turn the robot 360/7 degrees to the right
-		
+		bb8.turn(360/7);
 			// 8. Set the pen width to i
-			
+		bb8.setPenWidth(i);
+		}
 	}
 }
